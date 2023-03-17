13ae27067ca5f0cf7f24749056ad874be26ffa96
ngachadavid <davidngacha01@gmail.com>
2023-03-17 23:42:42 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: ['Fiction', "Non-Fiction"]
+    validate :title_does_not_contain
+
+    def title_does_not_contain
+      unless title.present? && title.match?(/Won't Believe|Secret|Top [number]|Guess/)
+        errors.add(:title, "some words are missing!")
+      end
+    end
+  end
+# class Post < ApplicationRecord
+#     validates :title, presence: true
+#     validates :content, length: {minimum:250}
+#     validates :summary, length: {minimum:250}
+#     validates :category, inclusion: ['Fiction', 'Non-Fiction']
+#     validate :title_does_not_contain
+
+#     def title_does_not_contain
+#         unless title.present? && title.match? (/Won't Believe|Secret|Top [number]|Guess/)
+#             errors.add(:title, "some words are missing!")
+#         end
+#     end
+# end
